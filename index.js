d67ce0a181fdc454cd0bc7e3c26699feed43e889
Nma Shawkat <nmashawket@gmail.com>
2022-11-14 13:58:35 +0300

    d

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    // create a new, empty array to hold the messages;
+    let newArr = [];
+  
+    // iterate through the input array
+    for (let i = 0; i < names.length; i++) {
+      // inside the loop, build out the 'thank you' message for each name
+      newArr.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+  
+    return newArr;
+  }
+  
+  function countDown(n) {
+    while (n > 0) {
+      console.log(n);
+      n -= 1;
+    }
+    console.log(n);
+  }
+  console.log(countDown(10));
